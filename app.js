3ad9f1b7c5759d7842b82e1e4e1887fcc46a5fe7
Rao-Umair-Ali <ruarua682@gmail.com>
2022-10-23 22:33:35 +0500

    Data types
    there are three types of data
    strings
    number
    boolean

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-alert("hello");
-alert("first you must link java script file with Html file than in js file you wrote alert() and in parameter you write in it with your own");
+// =======================Data Types=======================
+// 1 String "ABCD or Any Alphabates" with "" or ''
+// 2 Number  1457382 or -1-3-5-8 or 0.9 0.7 0.4 
+// 3 Boolean true or false 
+//  Var is for storing any thing 
+
+
+
+
+//====================string==============================
+//Note: In writing string we put all of our wording in  this "" 
+var name = "my name is umair rao "
+//console.log is also used as like alert we can see this by inspecting in browser in console 
+console.log(name);
+
+
+
+//====================Number===============================
+// Note: In writing number we dont put number in strings
+var age = 18
+console.log(age);
+
+
+// ==================Boolean==============================
+// Note: There are two things in boolean true and false
+var bool = false 
+console.log(bool);
+
+
+
+
+
+
+
+
+
+
+
